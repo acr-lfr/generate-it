743574f8ee4dffcc6fd60670b4c549ba4eb5debc
Eli Revah <revah@acrontum.de>
2019-07-26 17:36:27 +0200

    feat: added segment minimum join count

--- commander.js
+++ commander.js
@@ -17,6 +17,7 @@ module.exports = () => {
     .option('-m, --mocked', 'If passed, the domains will be configured to return dummy content.')
     .option('-o, --output <outputDir>', 'directory where to put the generated files (defaults to current directory)', process.cwd(), parseOutput)
     .option('-t, --template <template>', 'template to use (es6 or typescript)', 'es6')
+    .option('-s, --segments-count <segmentsCount>', 'minimum number of segments to start merging', 1)
     .option('-i, --ignored-modules <ignoredModules>', 'ignore the following type of modules (routes, controllers, domains, validators, transformers) in case they already exist (separated by commas)')
     .parse(process.argv)
 
