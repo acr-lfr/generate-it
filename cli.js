871a0b7a25fdc5fd81b8762c52e7fe0e41d11ab2
John Carmichael <carmichael.john@aol.com>
2019-08-20 13:07:14 +0200

    fix: git fetch url

--- cli.js
+++ cli.js
@@ -8,14 +8,14 @@ const inquirer = require('inquirer')
 console.log('All looks good, give me a moment to generate the server for you!'.yellow)
 global.verboseLogging = (o) => {
   if (cli.program.verbose) {
-    console.log(JSON.stringify(o, '', 2))
+    console.log(JSON.stringify(o, undefined, 2))
   }
 }
 
 const go = (mockServer) => {
   codegen({
     swaggerFilePath: cli.swaggerFile,
-    target_dir: cli.program.output,
+    targetDir: cli.program.output,
     template: cli.program.template,
     segmentsCount: +cli.program.segmentsCount,
     handlebars_helper: cli.program.handlebars ? path.resolve(process.cwd(), cli.program.handlebars) : undefined,
