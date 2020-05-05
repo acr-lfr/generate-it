7a38ce5c46b590eedf9ec9afefc1dad7ad3a1ff9
John Carmichael <john@johnine.org>
2020-05-05 17:54:21 +0200

    feat: pathMethodsHaveAttr extention to find nested objects

--- src/lib/template/helpers/pathMethodsHaveAttr.ts
+++ src/lib/template/helpers/pathMethodsHaveAttr.ts
@@ -1,17 +1,40 @@
+import * as _ from 'lodash';
 import { Operation, Operations } from '@/interfaces/Operations';
 import isValidMethod from '@/lib/template/helpers/isValidMethod';
 
-export default (operations: Operations, attr: string): boolean => {
+export default (operations: Operations, attr: string, nestedPath?: string): boolean => {
   let found = false;
-  // console.log(JSON.stringify(operations, null, 2));
-  operations.forEach((op: Operation) => {
+  for (let key in operations) {
+    const op: Operation = operations[key];
     for (const method in op.path) {
       if (isValidMethod(method)) {
         if (op.path[method][attr]) {
-          found = true;
+          if (!nestedPath) {
+            found = true;
+          } else {
+            const nestedParts = nestedPath.split('.');
+            let objectToLookIn = op.path[method][attr];
+            nestedParts.forEach((part) => {
+              if (objectToLookIn) {
+                if (Array.isArray(objectToLookIn)) {
+                  objectToLookIn = objectToLookIn.find(item => {
+                    return item[part] !== undefined;
+                  });
+                  if (objectToLookIn) {
+                    objectToLookIn = objectToLookIn[part];
+                  }
+                } else {
+                  objectToLookIn = _.get(objectToLookIn, part);
+                }
+              }
+            });
+            if (objectToLookIn !== undefined) {
+              found = true;
+            }
+          }
         }
       }
     }
-  });
+  }
   return found;
 };
