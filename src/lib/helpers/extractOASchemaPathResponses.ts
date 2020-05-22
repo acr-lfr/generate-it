0d1443e4ac5f892239508f96db2f842f10bbeecf
John <john@johnine.org>
2020-05-22 16:22:50 +0200

    feat: check if input could be a simple oa schema object, remove unused npm package

--- src/lib/helpers/extractOASchemaPathResponses.ts
+++ src/lib/helpers/extractOASchemaPathResponses.ts
@@ -12,5 +12,7 @@ export default (input: any) => {
       }
     }
   }
-  return (input && input.schema) ? input.schema : {};
+  // We also check if the input contains any valid OA schema by looking for type or properties in the provided object
+  // The typical use case here if for async api payloads
+  return (input && (input.type || input.properties)) ? input : {};
 };
