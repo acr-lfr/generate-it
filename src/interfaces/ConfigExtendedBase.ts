f3d3171fb57b812d492bbf7b1d84abf8224d5c63
Patrick McGowan <patrick.mcgowan@acrontum.de>
2021-01-18 19:17:49 +0100

    "fix" async typings for now

--- src/interfaces/ConfigExtendedBase.ts
+++ src/interfaces/ConfigExtendedBase.ts
@@ -3,10 +3,13 @@ import { NodegenRc } from '@/interfaces/NodegenRc';
 import { Package } from '@/interfaces/Package';
 import { Swagger } from '@/interfaces/Swagger';
 
+// TODO
+export type AsyncApi = Record<string, any>;
+
 export interface ConfigExtendedBase extends Config {
   nodegenRc?: NodegenRc;
   interfaceStyle?: string;
   templates?: string;
-  swagger?: Swagger;
+  swagger?: Swagger | AsyncApi;
   package?: Package;
 }
