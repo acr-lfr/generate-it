829c7eb5799d120e646db064f908eb477c41216c
Patrick McGowan <patrick.mcgowan@acrontum.de>
2021-01-21 16:14:45 +0100

    if res is passed to domain, mock will use it to respond

--- src/lib/template/helpers/mockOutput.ts
+++ src/lib/template/helpers/mockOutput.ts
@@ -1,13 +1,19 @@
 import extractOASchemaPathResponses from '@/lib/helpers/extractOASchemaPathResponses';
 
-const dummyGenerate = (schema: any) => {
-  return schema ? 'return mockItGenerator(' + JSON.stringify(schema) + ')' : undefined;
+const dummyGenerate = (schema: any, path: any) => {
+  if (schema) {
+    if (path['x-passResponse']) {
+      return `res.end(JSON.stringify(mockItGenerator(${JSON.stringify(schema)}))); return Promise.resolve(null);`;
+    } else {
+      return `return mockItGenerator(${JSON.stringify(schema)})`;
+    }
+  }
 };
 
 export default (path: any, mockServer: boolean) => {
   if (mockServer) {
     return (path && path.responses) ?
-      dummyGenerate(extractOASchemaPathResponses(path.responses)) :
+      dummyGenerate(extractOASchemaPathResponses(path.responses), path) :
       undefined;
   } else {
     return 'return {}';
