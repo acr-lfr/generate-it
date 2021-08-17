d4238f207000410b9d45cf4f94d6df7f5d02c35c
p-mcgowan <mcgowanapat+git@gmail.com>
2021-08-17 12:51:38 +0200

    preserve default behaviour - required: true is tested instead of truthy required: [params] array

--- src/lib/helpers/SwaggerUtils.ts
+++ src/lib/helpers/SwaggerUtils.ts
@@ -46,7 +46,7 @@ class SwaggerUtils {
     }
     const {paramTypeKey} = options;
     let validationText = param.name ? `'${param.name}'` + ':' : '';
-    const isRequired = (options.requiredFields && options.requiredFields.includes(param.name))/* || param.required*/;
+    const isRequired = (options.requiredFields && options.requiredFields.includes(param.name)) || param.required === true;
     const type = param.type || param.schema.type;
 
     const nullable = (paramTypeKey === ParamTypeKey.body && param['x-nullable'] !== false && param.nullable !== false) ? '.allow(null)' : '';
