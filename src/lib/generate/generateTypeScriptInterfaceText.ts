559daf54ea696c660f5f3a28ef33da2e3271f1aa
Iskren Slavov <iskren.s@gmail.com>
2022-07-26 17:27:52 +0300

    feat: Create a nullable workaround option until we get rid of quicktype.

--- src/lib/generate/generateTypeScriptInterfaceText.ts
+++ src/lib/generate/generateTypeScriptInterfaceText.ts
@@ -1,12 +1,54 @@
 import { LINEBREAK } from '@/constants/cli';
 import { ConfigExtendedBase } from '@/interfaces';
 import { GenerateTypeScriptInterfaceText } from '@/interfaces/GenerateTypeScriptInterfaceText';
+import { JSONSchema } from 'json-schema-ref-parser';
 
 const { InputData, JSONSchemaInput, JSONSchemaStore, quicktype } = require('quicktype/dist/quicktype-core');
 
 const countNoOfMatches = (name: string, line: string): number => {
   const regex = new RegExp(name, 'gi');
   return ((line || '').match(regex) || []).length;
+};
+
+const fixNullableProperties = (objectProperties: Record<string, any>): Record<string, any> => {
+  return Object.entries(objectProperties).reduce((obj, [key, value]) => ({
+    ...obj,
+    [key]: value?.type && (value.nullable || value['x-nullable'])
+      ? {
+        oneOf: [
+          { type: value.type },
+          { type: 'null' }
+        ]
+      }
+      : value
+  }), {});
+};
+
+const fixNullableTypes = (definitionObject: JSONSchema, enableNullableWorkaround?: boolean): JSONSchema => {
+  if (!enableNullableWorkaround) {
+    return definitionObject;
+  }
+
+  const unsafeDefinition = definitionObject as any;
+  const arrayItems = definitionObject?.items as JSONSchema;
+
+  if (definitionObject?.type === 'array' && arrayItems?.type === 'object' && arrayItems.properties) {
+    (definitionObject.items as JSONSchema).properties = fixNullableProperties(arrayItems.properties);
+  } else if (definitionObject?.type === 'object' && definitionObject.properties) {
+    definitionObject.properties = fixNullableProperties(definitionObject.properties);
+  } else if (definitionObject?.type && (unsafeDefinition.nullable || unsafeDefinition['x-nullable'])) {
+    const { type, ...definition } = unsafeDefinition;
+
+    return {
+      oneOf: [
+        { type },
+        { type: 'null' }
+      ],
+      ...definition
+    };
+  }
+
+  return definitionObject;
 };
 
 export default async (name: string, schema: string, config: ConfigExtendedBase): Promise<GenerateTypeScriptInterfaceText> => {
@@ -16,7 +58,9 @@ export default async (name: string, schema: string, config: ConfigExtendedBase):
     schema: JSON.stringify({
       type: 'object',
       properties: {
-        [name]: schema ? JSON.parse(schema) : schema,
+        [name]: schema
+          ? fixNullableTypes(JSON.parse(schema), config.nodegenRc.enableNullableWorkaround)
+          : schema
       },
     }),
   });
