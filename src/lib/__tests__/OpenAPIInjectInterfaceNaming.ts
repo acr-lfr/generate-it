df76d00e121005e6596dd8679aff5878a38b6232
Iskren Slavov <iskren.s@gmail.com>
2022-11-18 15:02:26 +0200

    fix: Improve required field handling in the request parameters object schema generation.

--- src/lib/__tests__/OpenAPIInjectInterfaceNaming.ts
+++ src/lib/__tests__/OpenAPIInjectInterfaceNaming.ts
@@ -1,19 +1,62 @@
-
-import { ConfigExtendedBase } from '@/interfaces';
 import OpenAPIInjectInterfaceNaming from '@/lib/openapi/OpenAPIInjectInterfaceNaming';
 import { mockConfig } from '@/__mocks__/mockConfig';
+import { JSONSchema } from 'json-schema-ref-parser';
 
 const swagger2obj = {
   swagger: '2.0',
 };
 const initialisedSwagger = new OpenAPIInjectInterfaceNaming(swagger2obj, mockConfig);
+
 const openApiobj = {
   openapi: '3.0.0',
 };
 const initialisedOpenApi = new OpenAPIInjectInterfaceNaming(openApiobj, mockConfig);
 
+const requestParam: JSONSchema = {
+  in: 'query',
+  name: 'term',
+  schema: { type: 'string' },
+  description: 'Search term',
+  type: 'string'
+};
+
 describe('Initialise and validate file type checks', () => {
-  it('Should inject an open api object', () => {
+  it('Should inject a Swagger object', () => {
     expect(initialisedSwagger.apiObject).toEqual(swagger2obj);
+  });
+
+  it('Should inject an OpenAPI object', () => {
+    expect(initialisedOpenApi.apiObject).toEqual(openApiobj);
+  });
+
+  it('Should convert required parameters to proper schema object', () => {
+    expect(
+      initialisedOpenApi.convertRequestParamsToSchemaObject({
+        term: requestParam
+      })
+    ).toEqual({
+      type: 'object',
+      required: [],
+      properties: {
+        term: requestParam
+      }
+    });
+  });
+
+  it('Should convert non required parameters to proper schema object', () => {
+    expect(
+      initialisedOpenApi.convertRequestParamsToSchemaObject({
+        term: {
+          required: true,
+          ...requestParam
+        }
+      })
+    ).toEqual({
+      type: 'object',
+      required: ['term'],
+      properties: {
+        term: requestParam
+      }
+    });
   });
 });
